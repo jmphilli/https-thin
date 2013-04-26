c2dabfb5430b7287094c7021add50d9e59486be4
justin <jjustinphillipss@gmail.com>
2013-04-25 20:17:57 -0700

    ok, so i just included everything, which i consider bad. i'll remove product files as i learn about this mumbo jumbo. for now it's ok. .gitignore will be edited accordingly. probably don't clone this commit hash.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e4f9e92aa70aeb388fa349cb357ea9e0b2132e9e6b961ef98c080f616de68f4fff289de317cc01659582314ed52cde554db3ce1ea481baea6417c5d5e2015106'
