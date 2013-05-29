4a592b4bfebc3d3485baba6ba81f9eb93826fd9a
justin <jjustinphillipss@gmail.com>
2013-05-29 11:06:14 -0700

    took me a while to get back over here, but i finally created user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name  "Justin Phillips"
+    email "justinphillips@stravabeer.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
