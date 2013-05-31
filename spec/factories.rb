7228bc688f1592d880726b96a47d633e0d809cfe
Justin Phillips <justin.phillips@locusenergy.com>
2013-05-31 11:24:45 -0700

    users page, paginated

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name  "Justin Phillips"
-    email "justinphillips@stravabeer.com"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com" }
     password "foobar"
     password_confirmation "foobar"
   end
